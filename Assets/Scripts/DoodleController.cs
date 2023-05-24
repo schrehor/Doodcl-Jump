21592f17cd2aaf1eeb6a85b58af4aff89837cdf8
schrehor <rehor.skola@gmail.com>
2023-05-25 00:58:22 +0200

    Hladam ten spravny odraz, chapeme sa

---
+++ Assets/Scripts/DoodleController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoodleController : MonoBehaviour
+{
+    [SerializeField] private float jumpStrength;
+
+    private Rigidbody2D _rigidbody;
+    
+    void Start()
+    {
+          _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+             _rigidbody.velocity = Vector2.up * jumpStrength;
+        }
+       
+    }
+}
