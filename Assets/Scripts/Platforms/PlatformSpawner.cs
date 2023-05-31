c154fdffea0dd92f170c3e2f3fe9057d100b1262
schrehor <rehor.skola@gmail.com>
2023-05-31 18:58:12 +0200

    Presunutie zakladnych premennych do Awake

--- Assets/Scripts/Platforms/PlatformSpawner.cs
+++ Assets/Scripts/Platforms/PlatformSpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,27 @@ public class PlatformSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject platformPrefab;
     [SerializeField] private int initialPlatformCount;
+
+    private float _minY;
+    private float _minX;
+    private float _maxY;
+    private float _maxX;
+
+    private void Awake()
+    {
+        float lowestPointY = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).y;
+        float highestPointY = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, 0)).y;
+        float lowestPointX = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).x;
+        float highestPointX = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x;
+
+        float platformWidth = platformPrefab.GetComponent<SpriteRenderer>().bounds.extents.x;
+        float platformHeight = platformPrefab.GetComponent<SpriteRenderer>().bounds.extents.y;
+
+        _minX = lowestPointX + platformWidth;
+        _minY = lowestPointY + platformHeight;
+        _maxX = highestPointX - platformWidth;
+        _maxY = highestPointY - platformHeight;
+    }
 
     public void Initialize(PlayerController player)
     {
@@ -16,22 +38,14 @@ public class PlatformSpawner : MonoBehaviour
 
     private void SpawnInitialPlatforms(float offset)
     {
-        float lowestPointy = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).y;
-        float highestPointy = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, 0)).y;
-        float lowestPointx = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).x;
-        float highestPointx = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x;
+        Instantiate(platformPrefab, new Vector3(_minX, _minY, 0), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(_minX, _maxY, 0), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(_maxX, _minY, 0), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(_maxX, _maxY, 0), Quaternion.identity);
 
-        float platformWidth = platformPrefab.GetComponent<SpriteRenderer>().bounds.extents.x;
-        float platformHeight = platformPrefab.GetComponent<SpriteRenderer>().bounds.extents.y;
-        
-        Instantiate(platformPrefab, new Vector3(lowestPointx + platformWidth, lowestPointy + platformHeight, 0), Quaternion.identity);
-        Instantiate(platformPrefab, new Vector3(lowestPointx + platformWidth, highestPointy - platformHeight, 0), Quaternion.identity);
-        Instantiate(platformPrefab, new Vector3(highestPointx - platformWidth, lowestPointy + platformHeight, 0), Quaternion.identity);
-        Instantiate(platformPrefab, new Vector3(highestPointx - platformWidth, highestPointy - platformHeight, 0), Quaternion.identity);
-        
-        Instantiate(platformPrefab, new Vector3(0, lowestPointy + platformHeight, 0), Quaternion.identity);
-        Instantiate(platformPrefab, new Vector3(1, lowestPointy + platformHeight + offset, 0), Quaternion.identity);
-        Instantiate(platformPrefab, new Vector3(2, lowestPointy + platformHeight + offset * 2, 0), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(0, _minY, 0), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(1, _minY + offset, 0), Quaternion.identity);
+        Instantiate(platformPrefab, new Vector3(2, _minY + offset * 2, 0), Quaternion.identity);
     }
 
 }
