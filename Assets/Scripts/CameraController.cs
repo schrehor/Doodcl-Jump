120ae2db4521436463d5f4f16c9eaf3d2d9a7742
schrehor <rehor.skola@gmail.com>
2023-06-04 23:29:38 +0200

    Uvidime co toto spravi

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,21 +6,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform player;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    // Late update aby sa kamera pohla az po tom ako sa pohne hrac
+    void LateUpdate()
     {
         if (player.position.y > transform.position.y)
         {
             var position = transform.position;
             position = new Vector3(position.x, player.position.y, position.z);
-            transform.position = position;
+            // SmoothDamp keby boli problemy
+            transform.position = Vector3.Lerp(transform.position, position, 0.3f);
         }
     }
 }
