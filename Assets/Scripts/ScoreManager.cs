5ac47a3a4eff58fc5b8cf1fe045c45544cf993a7
schrehor <rehor.skola@gmail.com>
2023-06-05 13:38:19 +0200

    Co tak kukate??

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
+    public Text scoreText;
+
+    private float _topScore = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText.text = Mathf.Round(_topScore).ToString(CultureInfo.InvariantCulture);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y > _topScore)
+        {
+            _topScore = transform.position.y;
+        }
         
+        scoreText.text = Mathf.Round(_topScore).ToString(CultureInfo.InvariantCulture);;
     }
 }
