2b955abb23768f1dde1721392e9637f2e49a3e27
schrehor <rehor.skola@gmail.com>
2023-06-06 12:02:36 +0200

    Znicene platformy na pana

--- Assets/Scripts/Platforms/PlatformBreakable.cs
+++ Assets/Scripts/Platforms/PlatformBreakable.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlatformBreakable : Platform
+namespace Platforms
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlatformBreakable : Platform
     {
+        private Animator _animator;
         
-    }
+        private static readonly int Break = Animator.StringToHash("Break");
 
-    // Update is called once per frame
-    void Update()
-    {
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
         
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                if (other.GetComponent<Rigidbody2D>().velocity.y < 0)
+                    _animator.SetTrigger(Break);
+            }
+        }
+        
+        private void DisablePlatform()
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
